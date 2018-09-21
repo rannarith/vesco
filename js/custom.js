097b5b22d1830c37a2b3592f6e59ea20f994581f
Ran Narith <rannarith@gmail.com>
2018-09-21 07:38:08 +0700

    Add work section

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,16 @@ $(function () {
     // WOW animation when scroll
     new WOW().init();
 })
+/*===================================================================
+                        WORK
+===================================================================*/
+$(function () {
+    $('#work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true
+        }
+    });
+})
