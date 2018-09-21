ff8ab6465ee48386550f1313a7393aa932ac2bea
DELLXPS\Premium Auto import <rannarith@gmail.com>
2018-09-21 18:48:19 +0700

    add Team

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,16 @@ $(function () {
         }
     });
 })
+
+/*===================================================================
+                        TEAM
+===================================================================*/
+$(function () {
+    $('#team-members').owlCarousel({
+        items:3,
+        autoplay:true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
